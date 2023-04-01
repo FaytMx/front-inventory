959aa39d05681cf6418c7900ed2ff54c40a52220
FaytMX <silverzero55@gmail.com>
2023-03-31 21:52:24 -0600

    sidenav

---
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,23 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.css'],
+})
+export class SidenavComponent implements OnInit {
+	mobileQuery: MediaQueryList;
+
+  menuNav = [
+    { name: 'Home', icon: 'home', route: 'home' },
+    { name: 'Categorías', icon: 'category', route: 'category' },
+    { name: 'Productos', icon: 'production_quantity_limits', route: 'product' },
+  ];
+
+	constructor(media: MediaMatcher) {
+		this.mobileQuery = media.matchMedia('(max-width: 600px)');
+	}
+
+	ngOnInit(): void {}
+}
