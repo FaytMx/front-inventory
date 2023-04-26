331e7628e5dcfcee8a62fd21d958e751fcf3cf27
FaytMX <silverzero55@gmail.com>
2023-04-25 20:56:18 -0600

    cerrar session

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
 	selector: 'app-sidenav',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 	mobileQuery: MediaQueryList;
+	username: string = '';
 
 	menuNav = [
 		{ name: 'Home', icon: 'home', route: 'home' },
@@ -19,9 +21,18 @@ export class SidenavComponent implements OnInit {
 		},
 	];
 
-	constructor(media: MediaMatcher) {
+	constructor(
+		media: MediaMatcher,
+		private keycloackService: KeycloakService
+	) {
 		this.mobileQuery = media.matchMedia('(max-width: 600px)');
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.username = this.keycloackService.getUsername();
+	}
+
+	logout() {
+		this.keycloackService.logout();
+	}
 }
